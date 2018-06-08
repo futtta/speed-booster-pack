28646b21fa731d9050b70d58b1dbe12cab6ddb6b
Cristian Raiber <cristian@machothemes.com>
2018-06-08 18:12:36 +0300

    removed support tab

--- inc/template/options.php
+++ inc/template/options.php
@@ -34,7 +34,7 @@ if ( is_array( $option_arr ) && in_array( 'defer-from-footer', $option_arr ) ) {
 		<h2 class="nav-tab-wrapper wp-clearfix">
 			<a class="nav-tab" href="#general-options"><?php _e( 'General', 'sb-pack' ); ?></a>
 			<a class="nav-tab" href="#more-options"><?php _e( 'More Optimization', 'sb-pack' ); ?></a>
-			<a class="nav-tab" href="#support"><?php _e( 'Support', 'sb-pack' ); ?></a>
+			
 		</h2>
 
 		<form method="post" action="options.php">
@@ -249,86 +249,7 @@ if ( is_array( $option_arr ) && in_array( 'defer-from-footer', $option_arr ) ) {
 
 
 			</div><!--#image-options-->
-			<div id="support" class="sb-pack-tab">
 
-				<?php
-				if ( ! defined( 'WPINC' ) ) {
-					die;
-				}
-				?>
-				<div class="feature-section sbp-support">
-					<div class="row two-col center-support">
-
-						<h3>
-							<i class="dashicons dashicons-sos" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'Contact Support', 'sb-pack' ); ?>
-						</h3>
-						<p>
-							<i><?php esc_html_e( 'We offer support through WordPress.org\'s support forums.', 'sb-pack' ); ?></i>
-						</p>
-						<p>
-							<a target="_blank" class="button button-hero button-primary" href="<?php echo esc_url( 'https://wordpress.org/support/plugin/speed-booster-pack#new-post' ); ?>"><?php esc_html_e( 'Post on our support forums', 'sb-pack' ); ?></a>
-						</p>
-
-					</div>
-					<div class="row">
-						<h2 class="sbp-title">Looking for better WP hosting ?</h2>
-					</div>
-					<div class="row sbp-blog three-col">
-						<div class="col">
-							<h3>
-								<i class="dashicons dashicons-performance" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'Our Bluehost Hosting Review', 'sb-pack' ); ?>
-							</h3>
-							<p>
-								<i><?php esc_html_e( 'Despite its popularity, though, Bluehost often carries a negative perception among WordPress professionals. So as we dig into this Bluehost review, we\'ll be looking to figure out whether Bluehost\'s performance and features actually justify that reputation.', 'sb-pack' ); ?></i>
-							</p>
-							<p>
-								<a target="_blank" href="<?php echo esc_url( 'https://www.machothemes.com/blog/bluehost-review/?utm_source=sbp&utm_medium=about-page&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'sb-pack' ); ?></a>
-							</p>
-						</div><!--/.col-->
-
-						<div class="col">
-							<h3>
-								<i class="dashicons dashicons-performance" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'Our InMotion Hosting Review', 'sb-pack' ); ?>
-							</h3>
-							<p>
-								<i><?php esc_html_e( 'InMotion Hosting is a popular independent web host that serves over 300,000 customers. They\'re notably not a part of the EIG behemoth (the parent company behind Bluehost, HostGator, and more), which is a plus in my book.', 'sb-pack' ); ?></i>
-							</p>
-							<p>
-								<a target="_blank" href="<?php echo esc_url( 'https://www.machothemes.com/blog/inmotion-hosting-review/?utm_source=sbp&utm_medium=about-page&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'sb-pack' ); ?></a>
-							</p>
-						</div><!--/.col-->
-
-						<div class="col">
-							<h3>
-								<i class="dashicons dashicons-performance" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'Our A2 Hosting Review', 'sb-pack' ); ?>
-							</h3>
-							<p>
-								<i><?php esc_html_e( 'When it comes to affordable WordPress hosting, A2 Hosting is a name that often comes up in various WordPress groups for offering quick-loading performance that belies its low price tag.', 'sb-pack' ); ?></i>
-							</p>
-							<p>
-								<a target="_blank" href="<?php echo esc_url( 'https://www.machothemes.com/blog/a2-hosting-review/?utm_source=sbp&utm_medium=about-page&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'sb-pack' ); ?></a>
-							</p>
-						</div><!--/.col-->
-					</div>
-				</div><!--/.feature-section-->
-
-				<div class="col-fulwidth feedback-box">
-					<h3>
-						<?php esc_html_e( 'Lend a hand & share your thoughts', 'sb-pack' ); ?>
-						<img src="<?php echo $this->plugin_url . 'inc/images/handshake.png'; ?>">
-					</h3>
-					<p>
-						<?php
-						echo vsprintf( // Translators: 1 is Theme Name, 2 is opening Anchor, 3 is closing.
-							__( 'We\'ve been working hard on making %1$s the best one out there. We\'re interested in hearing your thoughts about %1$s and what we could do to <u>make it even better</u>.<br/> <br/> %2$sHave your say%3$s', 'sb-pack' ), array(
-								'Speed Booster Pack',
-								'<a class="button button-feedback" target="_blank" href="http://bit.ly/feedback-speed-booster-pack">',
-								'</a>',
-							) );
-						?>
-					</p>
-				</div>
-			</div><!--#support-->
 
 			<div class="textright">
 				<hr />
